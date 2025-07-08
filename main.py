ff595a9096bfbc622518cb5d388612606ab29c60
JKYang01 <koreya991@gmail.com>
2025-07-08 16:14:52 -0400

    update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from data_model import Post
 app = FastAPI()
 
 @app.get("/login")
@@ -8,3 +10,19 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"data":"This is a list of your posts"}
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post.published)
+    return {"new_post":new_post}
+
+@app.get("/posts/{id}")
+def get_post(post_id: int):
+    return {"id":post_id}
+@app.patch("/posts/{id}")
+def update_post(post_id: int, new_post: Post):
+    return {"id":post_id, "new_post":new_post}
+
+@app.delete("/posts/{id}")
+def delete_post(post_id: int):
+    return {"id":post_id}
