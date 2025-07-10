aa599125e19394ec0c2ee751a56474174992713e
JKYang01 <koreya991@gmail.com>
2025-07-10 16:29:21 -0400

    update

--- data_model.py
+++ data_model.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 from typing import Optional
+from sqlalchemy import Column, Integer, String, JSON, TIMESTAMP, text, Boolean, ForeignKey, DECIMAL
 
 class Post(BaseModel):
+    id: Optional[int] = None  # Column(UUID(as_uuid=True),primary_key=True, index=True,default=uuid.uuid4)
     title: str
     content: str
     published: bool = True
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.params import Body
 from data_model import Post
+from random import randrange
 app = FastAPI()
+
+my_posts = {}
 
 @app.get("/login")
 async def root():
@@ -9,20 +12,31 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"This is a list of your posts"}
+    return {"data":my_posts}
 
 @app.post("/createposts")
 def create_posts(new_post: Post):
-    print(new_post.published)
-    return {"new_post":new_post}
+    post_dict = new_post.model_dump()
+    post_dict["id"] = int(randrange(0,100000))
+    my_posts.update({post_dict["id"]:post_dict})
+    print(post_dict)
+    return {"new_post":post_dict}
 
 @app.get("/posts/{id}")
-def get_post(post_id: int):
-    return {"id":post_id}
+def get_post(id:int):
+    post = find_post(id)
+    return {"post_detail":post}
 @app.patch("/posts/{id}")
-def update_post(post_id: int, new_post: Post):
-    return {"id":post_id, "new_post":new_post}
+def update_post(id, new_post: Post):
+    return {"id":id, "new_post":new_post}
 
 @app.delete("/posts/{id}")
-def delete_post(post_id: int):
-    return {"id":post_id}
+def delete_post(id:int):
+    return {"id":id}
+
+def find_post(id):
+    try:
+        post = my_posts[id]
+        return post
+    except IndexError:
+        raise HTTPException(status_code=404, detail="post not found")
